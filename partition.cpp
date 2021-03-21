12873a62151e5923ae70ba2427c3771b223a7abf
Christopher Nathan <christophernathan1217@gmail.com>
2021-03-21 12:44:31 -0600

    array with duplicate integers

--- partition.cpp
+++ partition.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 const int N = 5;
 
-int* partition(int C[N]){
+bool partition(int C[N]){
     int maxSum=0;
     for (int i=0;i<N;i++){ maxSum+=C[i]; }
-    bool T[maxSum];
-    for (int i=0;i<maxSum;i++){ T[i]=false; }
+    bool T[maxSum+1];
+    for (int i=0;i<=maxSum;i++){ T[i]=false; }
     T[0]=true;
     int R=0;
     for (int i=0;i<N;i++){
@@ -20,15 +20,43 @@ int* partition(int C[N]){
         }
     }
 
-    for (int i=0;i<maxSum;i++){
+    for (int i=0;i<=maxSum;i++){
         cout << i << ": " << T[i] << endl;
     }
+    return (maxSum%2==0 && T[maxSum/2]);
 }
+
+bool partition_duplicates(int* C, int* D){
+    int maxSum=0;
+    for (int i=0;i<N;i++){ maxSum+=D[i]*C[i]; }
+    bool T[maxSum+1];
+    for (int i=0;i<=maxSum;i++){ T[i]=false; }
+    T[0]=true;
+    int R=0;
+
+    for (int i=0;i<N;i++){
+        for (int j=R;j>=0;j--){
+            if (T[j]){
+                for (int k=1;k<=D[i];k++){
+                    T[j+k*C[i]]=true;
+                    if (j+k*C[i]>R){ R=j+k*C[i]; }
+                }
+            }
+        }
+    }
+
+    for (int i=0;i<=maxSum;i++){
+        cout << i << ": " << T[i] << endl;
+    }
+    return (maxSum%2==0 && T[maxSum/2]);
+}
+
 
 int main(){
 
     int C[N] = {1,2,5,7,9};
-    partition(C);
+    int D[N] = {1,1,1,1,7};
+    partition_duplicates(C,D);
 
 }
 
