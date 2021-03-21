e1d468dc6db55926987b28fb7b852450068ee1c6
Christopher Nathan <christophernathan1217@gmail.com>
2021-03-21 11:36:28 -0600

    testing initial partition implementation

--- partition.cpp
+++ partition.cpp
@@ -2,20 +2,33 @@
 #include <vector>
 using namespace std;
 
-const int N = 10;
-
-int C[N] = {1,2,3,4,5,6,7,8,9,10};
+const int N = 5;
 
 int* partition(int C[N]){
     int maxSum=0;
     for (int i=0;i<N;i++){ maxSum+=C[i]; }
     bool T[maxSum];
     for (int i=0;i<maxSum;i++){ T[i]=false; }
+    T[0]=true;
     int R=0;
-    
+    for (int i=0;i<N;i++){
+        for (int j=R;j>=0;j--){
+            if (T[j]==true){
+                T[j+C[i]]=true;
+                if (j+C[i]>R){ R=j+C[i]; }
+            }
+        }
+    }
+
+    for (int i=0;i<maxSum;i++){
+        cout << i << ": " << T[i] << endl;
+    }
 }
 
 int main(){
+
+    int C[N] = {1,3,5,7,9};
+    partition(C);
 
 }
 
